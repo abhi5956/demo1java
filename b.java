edc61c488075a0a7523b0eccbca9b1f769402043
abhi5956 <90917044+abhi5956@users.noreply.github.com>
2021-09-17 19:53:02 +0530

    Create b.java

---
+++ b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+       public class B {
+
+     private String str ="Hello";
+     public String publicstr ="hello";
+     
+     private void print() {
+    	
+    	 int a = 10;
+    	 int b = 10;
+    	 int sum = a+b;
+    	 System.out.println(sum);
+     }
+     public void add() {
+    	 print();
+    	 System.out.println("Addition: ");
+    	 
+     }
+       }
